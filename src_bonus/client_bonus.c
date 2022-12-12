cf1a828509cbb9db8a1e9284d473773d6ad3ea7b
Oscar Moreno Sancho <omoreno-@car6s4.42barcelona.com>
2022-12-12 10:52:37 +0100

    bonus takes changes in mand

--- src_bonus/client_bonus.c
+++ src_bonus/client_bonus.c
@@ -6,17 +6,41 @@
 /*   By: omoreno- <omoreno-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 11:46:04 by omoreno-          #+#    #+#             */
-/*   Updated: 2022/12/10 11:22:29 by omoreno-         ###   ########.fr       */
+/*   Updated: 2022/12/12 10:48:52 by omoreno-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk_bonus.h"
 
+static void	ft_send_bit_to_pid(int pid, int bit, useconds_t u)
+{
+	if (bit)
+		kill(pid, SIGUSR2);
+	else
+		kill(pid, SIGUSR1);
+	usleep(u);
+}
+
+static void	ft_send_byte_to_pid(int pid, char byte, useconds_t u)
+{
+	int	counter;
+	int	bit;
+
+	counter = 0;
+	while (! ft_take_bit_from_byte(byte, &bit, &counter))
+		ft_send_bit_to_pid(pid, bit, u);
+	ft_send_bit_to_pid(pid, bit, u);
+}
+
 int	main(int argc, char const *argv[])
 {
 	t_client_args	args;
+	int				i;
 
 	ft_get_client_args(argc, argv, &args);
+	i = 0;
+	while (args.msg_to_send[i])
+		ft_send_byte_to_pid(args.server_pid, args.msg_to_send[i++], 10);
 	free_x((void **)&args.msg_to_send);
 	return (0);
 }
